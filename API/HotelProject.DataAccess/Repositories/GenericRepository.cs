c579a7fbb2ebd7743ed849e4830f8fda7282f898
Enes Sariyildiz <ens.sryldz@gmail.com>
2024-01-31 14:31:32 +0300

    Generic repositories added

---
+++ API/HotelProject.DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using HotelProject.DataAccess.Abstract;
+using HotelProject.DataAccess.Concrete;
+
+namespace HotelProject.DataAccess.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly Context _context;
+
+        public GenericRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public void Insert(T t)
+        {
+           _context.Add(t);
+            _context.SaveChanges();
+        }
+
+        public void Remove(T t)
+        {
+            _context.Remove(t);
+            _context.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            _context.Update(t);
+            _context.SaveChanges();
+        }
+    }
+}
--- API/HotelProject.DataAccess/obj/Debug/net6.0/HotelProject.DataAccess.AssemblyInfo.cs
+++ API/HotelProject.DataAccess/obj/Debug/net6.0/HotelProject.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelProject.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9163b95b0bbe5baa44180b02d059c0928116751b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelProject.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelProject.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
